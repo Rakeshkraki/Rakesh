d54764d28851a9670eb122d1b1fae75566af9217
Rakesh K <97439056+Rakeshkraki@users.noreply.github.com>
2023-03-20 14:33:55 +0530

    Add files via upload

---
+++ MinMaxArr.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class MinMaxArr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the rows");
+        int row = sc.nextInt();
+
+        System.out.println("enter the columns");
+        int coln = sc.nextInt();
+
+        int arr[][] = new int[row][coln];
+
+        System.out.println("enter the array");
+        for(int i=0;i<row;i++) {
+            for (int j = 0; j < coln; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int max = arr[0][0];
+        int min = arr[0][0];
+        for(int i=0;i<row;i++) {
+            for (int j = 0; j < coln; j++) {
+                if(arr[i][j]>max){
+                    max = arr[i][j];
+                }
+                if(arr[i][j]<min){
+                    min = arr[i][j];
+                }
+            }
+        }
+        System.out.println("max elenent is: "+max);
+        System.out.println("min element is : "+min);
+    }
+}
